33c613849a6a1fd23a6a4dc91cdc935a41507948
Amaneyuu0515 <keigo.kaneko0515@gmail.com>
2025-06-04 10:56:32 +0900

    さ

--- work_01.py
+++ work_01.py
@@ -1,1 +1,16 @@
-print("test")
+import random
+r = random.randint(1,100)
+# print(r)
+w = int(input("1~100までの数字を当てよ"))
+print(f"入力された数字は{w}です。")
+if r < w:
+    print("50より小さいです")
+else:
+    ("51より大きいです")
+for i in range(5):
+    w = int(input())
+    if w == r:
+        print("success!!")
+        break
+    else:
+        print("failure")
