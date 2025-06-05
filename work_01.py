c045457f59f6964eeddd8c559f386847ad0c64f7
Amaneyuu0515 <keigo.kaneko0515@gmail.com>
2025-06-05 11:15:15 +0900

    最初のファイル

--- work_01.py
+++ work_01.py
@@ -1,16 +1,49 @@
 import random
+
 r = random.randint(1,100)
 # print(r)
-w = int(input("1~100までの数字を当てよ"))
-print(f"入力された数字は{w}です。")
-if r < w:
-    print("50より小さいです")
+p = "failure,onemore!"
+print("1~100の中から正解を当てろ")
+if r < 50:
+    print("50未満だ")
 else:
-    ("51より大きいです")
+    print("50以上だ")
 for i in range(5):
     w = int(input())
+    print(f"君の答えは{w}だ")
     if w == r:
-        print("success!!")
+        print("success!やるねぇ")
         break
     else:
-        print("failure")
+        print("failure,onemore!")
+        if w < r:
+            print(f"答えは{w}より大きいぞ、もっと大きく!")
+        else:
+            print(f"答えは{w}より小さいみたいだぞ？")
+
+if w == r:
+    pass
+else:
+    print("continuation? yes?/no?")
+    w = input()
+    if w =="yes":
+        print("1~100の中から正解を当てろ")
+    if r < 50:
+       print("50未満だ")
+    else:
+       print("50以上だ")
+for i in range(5):
+    w = int(input())
+    print(f"君の答えは{w}だ")
+    if w == r:
+        print("success!やるねぇ")
+        break
+    else:
+        print("failure,onemore!")
+        if w < r:
+            print(f"答えは{w}より大きいぞ、もっと大きく!")
+        else:
+            print(f"答えは{w}より小さいみたいだぞ？")
+else:
+    print("see you next game")
+print(f"ちな、答えは{r}だったぞ。")
