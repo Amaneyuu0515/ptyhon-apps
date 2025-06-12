578c402605b1b0a0d76d920fa7e5e73288a41186
Amaneyuu0515 <keigo.kaneko0515@gmail.com>
2025-06-12 10:35:33 +0900

    最初のファイル

--- work_02.py
+++ work_02.py
@@ -6,12 +6,13 @@ print("刹那の見切り!! 合図が出たらenterを押せ!")
 s = random.uniform(5,15)
 time.sleep(s)
 print("!!")
-start = time.time()
 e = input()
+start = time.time()
 end = time.time()
 time_diff = end - start
 time_diff = "{:.4}".format(time_diff)
 if time_diff < 0.01:
-   print("不正を検知しました。記録は無効化されます")
+    print("不正検知")
+    pass
 else:
-   print(f"反応速度{time_diff}秒")
+    print(f"反応速度{time_diff}")
