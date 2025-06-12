a1867c500846d3f55bbda9ab9a25dfdbb92166fe
Amaneyuu0515 <keigo.kaneko0515@gmail.com>
2025-06-12 12:09:34 +0900

    基本実装

--- work_02.py
+++ work_02.py
@@ -1,18 +1,20 @@
+
 import time
 import random
+b = random.randint(3,5)
+print(f"待ち時間: {b}秒")
 
-print("刹那の見切り!! 合図が出たらenterを押せ!")
+time.sleep(b)
 
-s = random.uniform(5,15)
-time.sleep(s)
-print("!!")
-e = input()
-start = time.time()
-end = time.time()
-time_diff = end - start
-time_diff = "{:.4}".format(time_diff)
-if time_diff < 0.01:
-    print("不正検知")
-    pass
-else:
-    print(f"反応速度{time_diff}")
+s= time.time()
+print(f"開始時:{s}秒")
+
+print("!!!!!")
+input("刹那の見切り!! 合図が出たらenterを押せ!")
+e = time.time()
+print(f"終了:{e}秒")
+k = e - s
+print(f"かかった時間{k}秒")
+if k < 0.01:
+    print("歪")   
+print("押された")
