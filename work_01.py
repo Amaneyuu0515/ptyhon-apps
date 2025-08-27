1503a5882f748397723b8e4ec4bb9a61f34eda25
Amaneyuu0515 <keigo.kaneko0515@gmail.com>
2025-08-27 12:13:23 +0900

    game

--- work_01.py
+++ work_01.py
@@ -1,49 +1,40 @@
 import random
 
-r = random.randint(1, 100)
-# print(r)
-p = "failure,onemore!"
-print("1~100の中から正解を当てろ")
-if r < 50:
-    print("50未満だ")
-else:
-    print("50以上だ")
-for i in range(5):
-    w = int(input())
-    print(f"君の答えは{w}だ")
-    if w == r:
-        print("success!やるねぇ")
-        break
-    else:
-        print("failure,onemore!")
-        if w < r:
-            print(f"答えは{w}より大きいぞ、もっと大きく!")
-        else:
-            print(f"答えは{w}より小さいみたいだぞ？")
 
-if w == r:
-    pass
-else:
-    print("continuation? yes?/no?")
-    w = input()
-    if w == "yes":
-        print("1~100の中から正解を当てろ")
-    if r < 50:
-        print("50未満だ")
-    else:
-        print("50以上だ")
-for i in range(5):
-    w = int(input())
-    print(f"君の答えは{w}だ")
-    if w == r:
-        print("success!やるねぇ")
-        break
-    else:
-        print("failure,onemore!")
-        if w < r:
-            print(f"答えは{w}より大きいぞ、もっと大きく!")
+while True:
+    # ランダムな数字を生成
+
+    a = random.randint(1, 100)  # 1から100のランダムな整数を生成
+    print(a)
+
+    result = False  # ゲームの結果を格納する変数
+    c = 0
+    for i in range(5):
+        c = c + 1
+        input_line = int(input("1から100の数字を入力してください: "))
+        if input_line == a:
+            print("正解")
+            result = True
+            break
         else:
-            print(f"答えは{w}より小さいみたいだぞ？")
-else:
-    print("see you next game")
-print(f"ちな、答えは{r}だったぞ。")
+            print("不正解")
+            if input_line < a:
+                if a - input_line > 10:
+                    print("10以上大きい")
+                else:
+                    print("もっと大きい数字ですが、かなり近いです")
+            else:
+                if input_line - a > 10:
+                    print("10以上小さいです")
+                else:
+                    print("もっと小さい数字ですが、かなり近いです")
+
+    if result:
+        print(f"ゲームに勝ちました！ あなたは{c}回目で正解しました。")
+    else:
+        print(f"ゲームに負けました。正解は{a}でした。")
+
+    ans = input("もう一度やる？(yes/no): ")
+    if ans != "yes":
+        print("seeyounextgame!")
+        break
